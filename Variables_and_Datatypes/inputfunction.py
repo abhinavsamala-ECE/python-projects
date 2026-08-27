e5164ffb76ed3cb5dc246bd66996674e251fc5fc
Abhinav reddy <abhinavsamala09@gmail.com>
2026-08-27 19:57:35 +0530

    input function

---
+++ Variables_and_Datatypes/inputfunction.py
@@ -0,0 +1,13 @@
+"""this
+is used for multi line comments"""
+
+name=input("Enter your name: ")
+#for string input
+
+age=int(input("Enter your age: "))
+#for integer input
+
+price=float(input("Enter the price: "))
+#for float input
+
+print("Name: ",name,"Age: ",age,"Price: ",price)
