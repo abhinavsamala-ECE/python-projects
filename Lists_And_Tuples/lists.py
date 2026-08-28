4121714d97cb0c6090fdf898d51a824817f46dec
Abhinav reddy <abhinavsamala09@gmail.com>
2026-08-28 10:48:23 +0530

    Final python projects commit of 28th august

--- Lists_And_Tuples/lists.py
+++ Lists_And_Tuples/lists.py
@@ -1,3 +1,14 @@
 numbers = [10, 20, 30, 40, 50]
 
 print(numbers)
+print(numbers[0])
+print(numbers[1])
+
+random = ["rithvik",3,'damn',3.14]
+print(random[2])
+
+string = "hello"
+print(string[2])
+str[2]='y' #assigning values in a STRING isn't allowed
+
+#but this is allowed in a list
