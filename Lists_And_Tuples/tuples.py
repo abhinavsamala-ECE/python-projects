2641a5b81727445f14aef8deb9c49c1a33a17e72
Abhinav reddy <abhinavsamala09@gmail.com>
2026-08-29 21:47:50 +0530

    list and tupples commit 2

---
+++ Lists_And_Tuples/tuples.py
@@ -0,0 +1,21 @@
+tup=(1,2,3,4)
+
+print(tup)
+print(type(tup))
+
+print(tup[0])
+print(tup[1])
+
+#to create a single element tupple it must have a comma after it, inside the brackets
+ple=(10,)
+
+print(type(ple))
+
+
+#TUPPLE SLICING
+
+print(tup[1:3])
+
+
+
+#LISTS AND TUPPLES CAN CONTAIN WORDS,STRINGS AND NUMBERS
---
+++ Lists_And_Tuples/tupple_functions.py
@@ -0,0 +1,5 @@
+a=(1,2,3,4,5,5)
+
+print(a.index(1)) #returns index of first occurence of the element we put in the brackets
+
+print(a.count(5)) #returns the number of times the element occurs in the tupple
