963e7fa4620a1ce3fbbde1b7787217e046ba1bfb
Abhinav reddy <abhinavsamala09@gmail.com>
2026-08-31 19:15:07 +0530

    x

--- dictionary_and_sets.py/set_functions.py
+++ dictionary_and_sets.py/set_functions.py
@@ -1,4 +1,5 @@
 set1={1,2,3,4,5}
+set2={6,7,8,9,10}
 
 set1.add(10) #adds an element
 print(set1)
@@ -6,9 +7,14 @@ print(set1)
 set1.remove(3) #removes an element
 print(set1)
 
-set1.clear() #removes all the elements in a set
-print(set1)
+x= set1.pop() #removes a random value
+print(x)
 
-set.pop() #removes a random value
+print(set1.union(set2)) #combines the values of both sets and makes a new set
+
+print(set1.intersection(set2)) #combines the common values and makes a new set
+
+"""set1.clear() #removes all the elements in a set
+print(set1)"""
 
 """SET is mutable(changeable) ELEMENT inside a set is IMMUTABLE"""
