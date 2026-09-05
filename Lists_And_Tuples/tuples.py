4210701fd37566f31237fbb47b71133ac934b18a
Abhinav reddy <abhinavsamala09@gmail.com>
2026-09-05 16:56:51 +0530

    Update tuples.py

--- Lists_And_Tuples/tuples.py
+++ Lists_And_Tuples/tuples.py
@@ -20,4 +20,4 @@ print(tup[1:3])
 
 #LISTS AND TUPPLES CAN CONTAIN WORDS,STRINGS AND NUMBERS
 
-"""Difference between list and tupples is that a list is meant to be changed, while a tupple cannot be changed."""
+"""Difference between list and tupples is that a list is mutable, while a tupple is immutable."""
